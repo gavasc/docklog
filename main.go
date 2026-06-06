39f5a24a20caa215c8311dd2c9d2c7fc35723d06
gv <gavasc@proton.me>
2026-06-06 19:04:30 -0300

    feat: adding 'config' command to open config file

--- main.go
+++ main.go
@@ -1,13 +1,33 @@
 package main
 
 import (
+	"os"
+	"os/exec"
+
 	"docklog/config"
 	"docklog/internal/watcher"
 )
 
 func main() {
-	// checks if tokens are set in the config file
-	config.CheckTokens()
+	if len(os.Args) < 1 {
+		// checks if tokens are set in the config file
+		config.CheckTokens()
 
-	watcher.Start()
+		watcher.Start()
+	} else if os.Args[1] == "config" {
+		editor := os.Getenv("EDITOR")
+		if editor == "" {
+			editor = "nvim" // fallback default
+		}
+
+		cmd := exec.Command(editor, "$HOME/.config/docklog/config.json")
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		if err := cmd.Run(); err != nil {
+			panic(err)
+		}
+
+		os.Exit(0)
+	}
 }
